05a8ed6d1cce9a6c4430099fdd9dffb691ab40d0
Coral Toucan <vipcalio@yahoo.com>
2023-12-03 23:38:29 +0000

    draft

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 # Makefile for MPI program
 
-CC=mpicc               # Compiler to use
+CC=mpic++              # Compiler to use
 CFLAGS=-Wall -O2       # Compiler flags, e.g., optimization and warning flags
-TARGET=mpi_hello_world # Target executable name
+TARGET=nbody # Target executable name
 SRC=main.cpp  # Source file
 
 all: $(TARGET)
@@ -10,8 +10,11 @@ all: $(TARGET)
 #$(TARGET): $(SRC)
 #	$(CC) $(CFLAGS) -o $(TARGET) $(SRC)
 
-bh: bh.cpp
-	g++ $(CFLAGS) -o bh bh.cpp
+nbody: bh.cpp BarnesHut.h
+	$(CC) $(CFLAGS) -o nbody main.cpp
+
+#bh: bh.cpp
+#	g++ $(CFLAGS) -o bh bh.cpp
 
 run: bh
 	./bh -i input/nb-10.txt -o output.txt -s 10 -t 1 -d  0.005
--- main.cpp
+++ main.cpp
@@ -38,92 +38,34 @@ void parseArguments(int argc, char *argv[], std::string &inputFile, std::string 
     }
 }
 
-MPI_Datatype createMPIBodyType() {
-    const int nitems = 5; // Number of elements in Body struct
-    int blocklengths[5] = {1, 1, 1, 1, 1}; // All elements are of length 1
-    MPI_Datatype types[5] = {MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE}; // All elements are of type MPI_DOUBLE
-    MPI_Datatype MPI_BODY_TYPE;
-    MPI_Aint offsets[5];
-
-    offsets[0] = offsetof(Body, x);
-    offsets[1] = offsetof(Body, y);
-    offsets[2] = offsetof(Body, mass);
-    offsets[3] = offsetof(Body, vx);
-    offsets[4] = offsetof(Body, vy);
-
-    MPI_Type_create_struct(nitems, blocklengths, offsets, types, &MPI_BODY_TYPE);
-    MPI_Type_commit(&MPI_BODY_TYPE);
-
-    return MPI_BODY_TYPE;
-}
-
-std::vector<Body> distributeBodies(const std::vector<Body>& all_bodies, int world_size, int world_rank, MPI_Datatype MPI_BODY_TYPE) {
+std::vector<Body> distributeBodies(const std::vector<Body>& all_bodies, int rank, int size) {
     int total_bodies = all_bodies.size();
-    int bodies_per_process = total_bodies / world_size;
-    int remaining_bodies = total_bodies % world_size;
+    int bodies_per_process = total_bodies / size;
+    int remaining_bodies = total_bodies % size;
 
-    // Calculate the actual number of bodies for each process
-    int local_body_count;
-    if (world_rank < remaining_bodies) {
-        local_body_count = bodies_per_process + 1;
-    } else {
-        local_body_count = bodies_per_process;
+    // Calculate the number of bodies for each process and the starting index for each chunk
+    std::vector<int> counts(size, bodies_per_process);
+    std::vector<int> displs(size, 0);
+
+    for (int i = 0; i < remaining_bodies; ++i) {
+        counts[i]++;
     }
 
-    std::vector<Body> local_bodies(local_body_count);
-
-    if (world_rank == 0) {
-        // Prepare counts and displacements for uneven distribution
-        std::vector<int> counts(world_size);
-        std::vector<int> displacements(world_size);
-
-        int current_displacement = 0;
-        for (int i = 0; i < world_size; ++i) {
-            counts[i] = (i < remaining_bodies) ? bodies_per_process + 1 : bodies_per_process;
-            displacements[i] = current_displacement;
-            current_displacement += counts[i];
-        }
-
-        MPI_Scatterv(all_bodies.data(), counts.data(), displacements.data(), MPI_BODY_TYPE, 
-                     local_bodies.data(), local_body_count, MPI_BODY_TYPE, 0, MPI_COMM_WORLD);
-    } else {
-        MPI_Scatterv(nullptr, nullptr, nullptr, MPI_BODY_TYPE, 
-                     local_bodies.data(), local_body_count, MPI_BODY_TYPE, 0, MPI_COMM_WORLD);
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        displs[i] = sum;
+        sum += counts[i];
     }
+
+    std::vector<Body> local_bodies(counts[rank]);
+
+    MPI_Scatterv(all_bodies.data(), counts.data(), displs.data(), MPI_BYTE, 
+                 local_bodies.data(), counts[rank] * sizeof(Body), MPI_BYTE, 
+                 0, MPI_COMM_WORLD);
 
     return local_bodies;
 }
 
-std::vector<Body> gatherBodies(const std::vector<Body>& local_bodies, int world_size, int world_rank, MPI_Datatype MPI_BODY_TYPE, int total_bodies) {
-    std::vector<Body> all_bodies;
-    if (world_rank == 0) {
-        // Allocate memory for all bodies in the root process
-        all_bodies.resize(total_bodies);
-    }
-
-    int local_body_count = local_bodies.size();
-    std::vector<int> counts(world_size);
-    std::vector<int> displacements(world_size);
-
-    // Gather the number of bodies each process will send
-    MPI_Gather(&local_body_count, 1, MPI_INT, counts.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-    if (world_rank == 0) {
-        // Calculate displacements
-        int current_displacement = 0;
-        for (int i = 0; i < world_size; ++i) {
-            displacements[i] = current_displacement;
-            current_displacement += counts[i];
-        }
-    }
-
-    // Gather all bodies to the root process
-    MPI_Gatherv(local_bodies.data(), local_body_count, MPI_BODY_TYPE, 
-                all_bodies.data(), counts.data(), displacements.data(), MPI_BODY_TYPE, 
-                0, MPI_COMM_WORLD);
-
-    return all_bodies;
-}
 
 void serializeQuadTree(QuadTreeNode* node, std::vector<char>& buffer) {
     if (node == nullptr) {
@@ -201,101 +143,107 @@ QuadTreeNode* deserializeQuadTree(const std::vector<char>& buffer, int& pos) {
     return node;
 }
 
+void updateBody(Body& body, double dt) {
+    // Calculate acceleration
+    double ax = body.fx / body.mass;
+    double ay = body.fy / body.mass;
+
+    // Update velocity
+    body.vx += ax * dt;
+    body.vy += ay * dt;
+
+    // Update position
+    body.x += body.vx * dt + 0.5 * ax * dt * dt;
+    body.y += body.vy * dt + 0.5 * ay * dt * dt;
+}
+
+void gatherBodies(std::vector<Body>& all_bodies, const std::vector<Body>& local_bodies, int world_rank, int world_size) {
+    int local_count = local_bodies.size();
+    std::vector<int> counts(world_size);
+    std::vector<int> displs(world_size);
+
+    // Gather the number of bodies each process is sending
+    MPI_Gather(&local_count, 1, MPI_INT, counts.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (world_rank == 0) {
+        // Calculate displacements and resize the all_bodies vector on the root process
+        int total_count = 0;
+        for (int i = 0; i < world_size; ++i) {
+            displs[i] = total_count;
+            total_count += counts[i];
+        }
+        all_bodies.resize(total_count);
+    }
+
+    // Gather the bodies from all processes
+    MPI_Gatherv(local_bodies.data(), local_count * sizeof(Body), MPI_BYTE,
+                all_bodies.data(), counts.data(), displs.data(), MPI_BYTE,
+                0, MPI_COMM_WORLD);
+}
+
 int main(int argc, char *argv[]) {
+    // Initialize MPI
     MPI_Init(&argc, &argv);
 
-    int world_size, world_rank;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int world_rank, world_size;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     std::string inputFile, outputFile;
     int steps;
     double theta, dt;
     bool visualization = false;
 
-    // Parse arguments and read input file
-    if (world_rank == 0) {
-        // Only root process handles file I/O
-        parseArguments(argc, argv, inputFile, outputFile, steps, theta, dt, visualization);
-    }
-
-    // Broadcast parsed arguments to all processes
-    MPI_Bcast(&steps, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&theta, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&dt, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
+    // Only rank 0 process should read the input file and distribute the bodies
     std::vector<Body> bodies;
     if (world_rank == 0) {
+        // Parse arguments and read input file
+        parseArguments(argc, argv, inputFile, outputFile, steps, theta, dt, visualization);
         bodies = readBodiesFromFile(inputFile);
     }
 
-    // Broadcast bodies to all processes
-    int num_bodies = bodies.size();
-    MPI_Bcast(&num_bodies, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    if (world_rank != 0) {
-        bodies.resize(num_bodies);
-    }
-    MPI_Bcast(bodies.data(), num_bodies * sizeof(Body), MPI_BYTE, 0, MPI_COMM_WORLD);
+    // Distribute the bodies among MPI processes
+    std::vector<Body> local_bodies = distributeBodies(bodies, world_rank, world_size);
 
-    auto start = std::chrono::high_resolution_clock::now();
+    // Begin timing the simulation
+    MPI_Barrier(MPI_COMM_WORLD);  // Synchronize before starting timing
+    double start_time = MPI_Wtime();
 
-    QuadTree tree(MAX_SIZE);
-    for (const auto& body : bodies) {
-        tree.insert(body);
-    }
+    std::vector<Body> all_updated_bodies;
 
-
+    // Perform simulation steps
     for (int step = 0; step < steps; ++step) {
-        // Root process builds the quadtree
-        if (world_rank == 0) {
-            for (const auto& body : bodies) {
-                tree.insert(body);
-            }
-
+        // Each process creates its local tree and computes forces
+        QuadTree local_tree(MAX_SIZE);
+        for (const auto& body : local_bodies) {
+            local_tree.insert(body);
         }
 
-        // Serialize and broadcast the quadtree structure
-        std::vector<char> serializedTree;
-        if (world_rank == 0) {
-            serializeQuadTree(tree.root, serializedTree);
+        for (auto& body : local_bodies) {
+            local_tree.updateForces(body, theta);
+            updateBody(body, dt);  // Update body position and velocity
         }
 
-        // Broadcast the size of the serialized data
-        int serializedSize = serializedTree.size();
-        MPI_Bcast(&serializedSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        // Gather updated bodies from all processes to process 0
+        gatherBodies(all_updated_bodies, local_bodies, world_rank, world_size);
+        // Synchronize all processes here if necessary
+        MPI_Barrier(MPI_COMM_WORLD);
 
-        // Resize buffer for other processes
-        if (world_rank != 0) {
-            serializedTree.resize(serializedSize);
-        }
-
-        // Broadcast the serialized tree
-        MPI_Bcast(serializedTree.data(), serializedSize, MPI_BYTE, 0, MPI_COMM_WORLD);
-
-        // Receive and deserialize quadtree at other processes
-        if (world_rank != 0) {
-            int position = 0;
-            QuadTreeNode* root = deserializeQuadTree(serializedTree, position);
-            // Now you have the reconstructed QuadTree on other processes
-        }
-
-        // Perform local computation on each process
-        for (auto& body : bodies) {
-            // Update forces and positions based on the received quadtree
-        }
-
-        // Collect updates from all processes at the root
-        // TODO: Use MPI_Gather or similar to collect the updated body data at root
+        // Placeholder for MPI gathering operation
     }
 
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = end - start;
+    // End timing the simulation
+    MPI_Barrier(MPI_COMM_WORLD);  // Synchronize before ending timing
+    double end_time = MPI_Wtime();
 
     if (world_rank == 0) {
-        std::cout << "Simulation time: " << elapsed.count() << " seconds." << std::endl;
-        writeBodiesToFile(outputFile, bodies);
+        // Process 0 writes results to file
+        writeBodiesToFile(outputFile, all_updated_bodies);
+        // Print elapsed time
+        std::cout << "Simulation time: " << (end_time - start_time) << " seconds." << std::endl;
     }
 
+    // Finalize MPI
     MPI_Finalize();
     return 0;
 }
